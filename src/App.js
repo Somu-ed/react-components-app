b0c4f690272e080a9d99793185e4e311455971d1
Soumyajeet Satpathy <satpathysoumyajeet.99@gmail.com>
2024-02-11 00:55:45 +0530

    Random Color Generator Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 import './App.css';
 // * Modules import
 import Accordion from './components/accordion/index';
+import RandomColor from './components/random-color-gen';
 
 function App() {
   return (
     <div className="App">
       {/* //* Accordion Component */}
       <Accordion />
+
+      {/* //* Random Color Generator Component */}
+      <RandomColor />
     </div>
   );
 }
